95879d7a1711ac3b34f1c841386b08af696e7311
JorgeJC98 <69093957+JorgeJC98@users.noreply.github.com>
2021-11-25 22:49:13 -0600

    portfolio divider in components

---
+++ resources/views/portfolio/components/about.blade.php
@@ -0,0 +1,42 @@
+<div class="container" data-aos="fade-up">
+  <div class="section-title">
+    <h2>About</h2>
+    <p>Developer FullStack. Experience in all stages of the life cycle
+      development of dynamic web projects. Strong backend experience.</p>
+  </div>
+
+  <div class="row">
+    <div class="col-lg-4">
+      <img src="{{ asset('assets_portfolio/img/profile-img.jpg') }}" class="img-fluid" alt="">
+    </div>
+    <div class="col-lg-8 pt-4 pt-lg-0 content">
+      <h3>Web Developer FullStack</h3>
+      <p class="fst-italic">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+        magna aliqua.
+      </p>
+      <div class="row">
+        <div class="col-lg-6">
+          <ul>
+            <li><i class="bi bi-chevron-right"></i> <strong>Birthday:</strong> <span>2 oct 1998</span></li>
+            <li><i class="bi bi-chevron-right"></i> <strong>Website:</strong> <span>www.example.com</span></li>
+            <li><i class="bi bi-chevron-right"></i> <strong>Phone:</strong> <span>+506 61953152</span></li>
+            <li><i class="bi bi-chevron-right"></i> <strong>City:</strong> <span>Costa Rica, Guanacaste, Nicoya</span></li>
+          </ul>
+        </div>
+        <div class="col-lg-6">
+          <ul>
+            <li><i class="bi bi-chevron-right"></i> <strong>Age:</strong> <span>23</span></li>
+            <li><i class="bi bi-chevron-right"></i> <strong>Degree:</strong> <span>University Bachelor</span></li>
+            <li><i class="bi bi-chevron-right"></i> <strong>Email:</strong> <span>albertop2203@gmail.com</span></li>
+            <li><i class="bi bi-chevron-right"></i> <strong>Freelance:</strong> <span>Available</span></li>
+          </ul>
+        </div>
+      </div>
+      <p>
+        Officiis eligendi itaque labore et dolorum mollitia officiis optio vero. Quisquam sunt adipisci omnis et ut. Nulla accusantium dolor incidunt officia tempore. Et eius omnis.
+        Cupiditate ut dicta maxime officiis quidem quia. Sed et consectetur qui quia repellendus itaque neque. Aliquid amet quidem ut quaerat cupiditate. Ab et eum qui repellendus omnis culpa magni laudantium dolores.
+      </p>
+    </div>
+  </div>
+</div>
